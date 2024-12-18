c5ea5450e2a4566308bd143bfaf1042a050236c0
hm sumon <hmfussumon7707@gmail.com>
2024-12-18 20:31:33 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+echo "git hub tui khub jalaili";
+
+
+
+?>
+
+
+ README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Hashansumon/afterSuccess.git
+git push -u origin main
