e38d5f089e346c5a88f5adad2364e6277eb32de3
hm sumon <hmfussumon7707@gmail.com>
2024-12-18 20:33:00 +0600

    dsd

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ echo "git hub tui khub jalaili";
 
 
  README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:Hashansumon/afterSuccess.git
-git push -u origin main
+
+
+
